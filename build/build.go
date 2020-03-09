2b7fc4746f4b0ee7ee6a193a71922b6626575e39
Jeff MONNIER <kessler.dev@gmail.com>
2020-03-09 17:53:32 +0100

    go lint

--- build/build.go
+++ build/build.go
@@ -1,9 +1,13 @@
 package build
 
+// Version in semver format
 var Version string = "0.0.1"
 
+// Revision git rev of code
 var Revision string
 
+// Time when app builded
 var Time string
 
+// User that built this app
 var User string
